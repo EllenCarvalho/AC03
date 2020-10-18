3088787b021456ba8a0f9b229bd75b007a75f8e4
ellencarvalho <ellen.carvalho@aluno.faculdadeimpacta.com.br>
2020-10-17 21:06:50 -0300

    AC03

---
+++ AC03.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def numeros_primos():
+    primos = []
+    contagem = 0
+    i = 0
+    saida = 'Primos: '
+    for numero in range(100):
+        for divisor in range(1, numero):
+            if numero % divisor == 0:
+                contagem += 1
+        if contagem == 1:
+             primos.append(numero)
+        contagem = 0
+    for a in primos:
+        i += 1
+        a = str(a)
+        saida += a
+        if i < len(primos):
+            saida += ', '
+    saida += '.'
+    return saida
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host = '0.0.0.0', port=port)
